0402b1803642a22093f38f325f85430adab15432
daniellejeromevinculado <daniellejerome.vinculado.cics@ust.edu.ph>
2022-02-10 22:21:47 +0800

    intent]

--- app/src/main/java/com/tolentino/dragonsis/ManagerMenu.java
+++ app/src/main/java/com/tolentino/dragonsis/ManagerMenu.java
@@ -10,6 +10,10 @@ import android.widget.Button;
 public class ManagerMenu extends AppCompatActivity {
 
     Button btn_useracc;
+    Button btn_inventory;
+    Button btn_products;
+    Button btn_pdf;
+    Button btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,10 @@ public class ManagerMenu extends AppCompatActivity {
         setContentView(R.layout.activity_manager_menu);
 
         btn_useracc = findViewById(R.id.btn_useracc);
+        btn_inventory = findViewById(R.id.btn_inventory);
+        btn_products = findViewById(R.id.btn_products);
+        btn_pdf = findViewById(R.id.btn_products);
+        btn_logout = findViewById(R.id.btn_logout);
 
         btn_useracc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +33,34 @@ public class ManagerMenu extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        btn_inventory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ManagerMenu.this, ViewInventory.class);
+                startActivity(i);
+            }
+        });
+        btn_products.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ManagerMenu.this, ViewProducts.class);
+                startActivity(i);
+            }
+        });
+        btn_pdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ManagerMenu.this, GeneratePdfReport.class);
+                startActivity(i);
+            }
+        });
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ManagerMenu.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
