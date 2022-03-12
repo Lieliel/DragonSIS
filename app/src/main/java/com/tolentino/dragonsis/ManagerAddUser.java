11730a779fc2082801994431b8c920d2b2afdd87
agpo3 <anthonygabriel.ortega.cics@ust.edu.ph>
2022-03-12 18:38:03 +0800

    check

--- app/src/main/java/com/tolentino/dragonsis/ManagerAddUser.java
+++ app/src/main/java/com/tolentino/dragonsis/ManagerAddUser.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -36,6 +38,23 @@ public class ManagerAddUser extends AppCompatActivity {
         edit_confirm_password = findViewById(R.id.edit_confirm_password);
         btn_user_submit = findViewById(R.id.btn_user_submit);
         db = new DbManager(this);
+
+        //Show Hide password using eye icon
+        ImageView imageViewShowHidePwd = findViewById(R.id.imageview_show_hide_pwd);
+        imageViewShowHidePwd.setImageResource(R.drawable.hide_pass);
+        imageViewShowHidePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edit_add_password.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())){
+                    edit_add_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    //Change Icon
+                    imageViewShowHidePwd.setImageResource(R.drawable.hide_pass);
+                } else
+                    edit_add_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.show_pass);
+            }
+        });
+
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, userTypeArray);
         spin_add_usertype.setAdapter(adapter);
@@ -70,4 +89,23 @@ public class ManagerAddUser extends AppCompatActivity {
         });
 
         }
+    private boolean validatePassword() {
+        String txt_password = edit_add_password.getText().toString().trim();
+        String txt_confirm = edit_confirm_password.getText().toString().trim();
+        if (txt_password.isEmpty()) {
+            edit_add_password.setText("Field can't be empty");
+            return false;
+        }  if (txt_password.length()<5) {
+            edit_add_password.setText("Password must be at least 5 characters");
+            return false;
+        }
+        if (!txt_password.equals(txt_confirm)) {
+            edit_confirm_password.setText("Password Would Not be matched");
+            return false;
+        }else {
+            edit_confirm_password.setText("Password Matched");
+            return true;
+        }
     }
+}
+
